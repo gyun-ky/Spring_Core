91c3841828e2354c31ddb7e14a1bafec7393f21c
gyun_ky <kiyoon9390@gmail.com>
2023-01-17 22:45:53 +0900

    v5 order tracetemplate으로 전환

--- src/main/java/hello/advanced/app/v5/OrderServiceV5.java
+++ src/main/java/hello/advanced/app/v5/OrderServiceV5.java
@@ -1,27 +1,30 @@
 package hello.advanced.app.v5;
 
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.callback.TraceCallback;
+import hello.advanced.trace.callback.TraceTemplate;
 import hello.advanced.trace.logtrace.LogTrace;
 import hello.advanced.trace.template.AbstractTemplate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class OrderServiceV5 {
 
     private final OrderRepositoryV5 orderRepository;
-    private final LogTrace trace;
+    private final TraceTemplate template;
+
+    public OrderServiceV5(OrderRepositoryV5 orderRepository, LogTrace trace) {
+        this.orderRepository = orderRepository;
+        this.template = new TraceTemplate(trace);
+    }
 
     public void orderItem(String itemId) {
 
-        AbstractTemplate<Void> template = new AbstractTemplate<Void>(trace) {
-            @Override
-            protected Void call() {
-                orderRepository.save(itemId);
-                return null;
-            }
-        };
+        template.execute("OrderService.orderItem()", () -> {
+            orderRepository.save(itemId);
+            return null;
+        });
 
-        template.execute("OrderService.orderItem()");
     }
 }
